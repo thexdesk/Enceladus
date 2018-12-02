b290be0015e1be92499de85a8a4ec9cbb0bf859a
Jacob Pratt <the.z.cuber@gmail.com>
2018-12-02 02:44:05 -0500

    Inline initialization functions, create index file for custom elements

---
+++ src/js/custom-elements/index.ts
@@ -0,0 +1,5 @@
+export { Header } from './x-header';
+export { YouTube } from './youtube-video';
+export { Links } from './x-links';
+export { Events } from './x-events';
+export { Sections } from './x-sections';
--- src/js/initialize.ts
+++ src/js/initialize.ts
@@ -1,16 +1,11 @@
-import { Header as HeaderElement } from './custom-elements/x-header';
-import { YouTube as YouTubeElement } from './custom-elements/youtube-video';
-import { Links as LinksElement } from './custom-elements/x-links';
-import { Events as EventsElement } from './custom-elements/x-events';
-import { Sections as SectionsElement } from './custom-elements/x-sections';
-
+import { Header, YouTube, Links, Events, Sections } from './custom-elements';
 import fetchival from 'fetchival';
 
-export const header_elem = document.querySelector<HeaderElement>('x-header')!;
-export const youtube_elem = document.querySelector<YouTubeElement>('youtube-video')!;
-export const links_elem = document.querySelector<LinksElement>('x-links')!;
-export const sections_elem = document.querySelector<SectionsElement>('x-sections')!;
-export const events_elem = document.querySelector<EventsElement>('x-events')!;
+export const header_elem = document.querySelector<Header>('x-header')!;
+export const youtube_elem = document.querySelector<YouTube>('youtube-video')!;
+export const links_elem = document.querySelector<Links>('x-links')!;
+export const sections_elem = document.querySelector<Sections>('x-sections')!;
+export const events_elem = document.querySelector<Events>('x-events')!;
 
 // Fetch data from API, insert into DOM
 
@@ -23,41 +18,27 @@ if (thread_id === null) {
   // tslint:disable-next-line no-unsafe-any
   fetchival(`http://localhost:3000/v1/thread/${thread_id[1]}?with=events`)
     .get()
-    .then((thread: APIThreadData<true>) => {
-      assign_header(thread);
-      assign_youtube(thread);
-      assign_reddit_id(thread);
-      assign_sections(thread);
-      assign_events(thread);
+    .then(({ launch_name, t0, youtube_id, post_id, sections }: APIThreadData<true>) => {
+      // assign header data
+      header_elem.launch_name = launch_name;
+      header_elem.t0 = t0;
+
+      // assign YouTube data
+      youtube_elem.video_id = youtube_id;
+
+      // assign links data
+      links_elem.reddit_id = post_id;
+
+      // assign sections data
+      sections
+        .filter(({ events }) => events.length === 0)
+        .forEach(data => sections_elem.add(data, false));
+      sections_elem.requestUpdate(); // tslint:disable-line no-floating-promises
+
+      // assign events data
+      const events_section = sections.find(section => section.events.length !== 0);
+      if (events_section === undefined) { return; }
+      events_section.events.forEach(data => events_elem.add(data, false));
+      events_elem.requestUpdate(); // tslint:disable-line no-floating-promises
     });
-}
-
-function assign_header({ launch_name, t0 }: APIThreadData<boolean>): void {
-  header_elem.launch_name = launch_name;
-  header_elem.t0 = t0;
-}
-
-function assign_youtube({ youtube_id }: APIThreadData<boolean>): void {
-  youtube_elem.video_id = youtube_id;
-}
-
-function assign_reddit_id({ post_id }: APIThreadData<boolean>): void {
-  links_elem.reddit_id = post_id;
-}
-
-function assign_sections({ sections }: APIThreadData<true>): void {
-  sections
-    .filter(({ events }) => events.length === 0)
-    .forEach(data => sections_elem.add(data, false));
-  sections_elem.requestUpdate(); // tslint:disable-line no-floating-promises
-}
-
-function assign_events({ sections }: APIThreadData<true>): void {
-  const events_section = sections.find(section => section.events.length !== 0);
-  if (events_section === undefined) {
-    return;
-  }
-
-  events_section.events.forEach(data => events_elem.add(data, false));
-  events_elem.requestUpdate(); // tslint:disable-line no-floating-promises
 }
