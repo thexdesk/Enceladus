77ea29e233c6ebb5a11975c57836ffbd8853b807
Jacob Pratt <the.z.cuber@gmail.com>
2018-12-08 03:06:53 -0500

    Switch from yarn to npm, upgrade to gulp 4 (and switch to TS)
    Quite a few gulp plugins don't have type declarations available, so I've
    had to `@ts-ignore` them. The ones that are currently available have
    been added as dev dependencies.

---
+++ gulpfile.ts
@@ -0,0 +1,136 @@
+/* eslint-env node */
+
+// we have to @ts-ignore imports that don't have type declarations available
+
+import * as del from 'del';
+import { dest, lastRun, parallel, series, src, watch as gulp_watch } from 'gulp';
+// @ts-ignore
+import * as rollup from 'gulp-better-rollup';
+import * as html_minifier from 'gulp-htmlmin';
+// @ts-ignore
+import * as postcss from 'gulp-postcss';
+import * as rename from 'gulp-rename';
+import * as sourcemaps from 'gulp-sourcemaps';
+// @ts-ignore
+import * as commonjs from 'rollup-plugin-commonjs';
+// @ts-ignore
+import * as node_resolve from 'rollup-plugin-node-resolve';
+// @ts-ignore
+import { terser } from 'rollup-plugin-terser';
+// @ts-ignore
+import * as typescript from 'rollup-plugin-typescript';
+import * as sw_precache from 'sw-precache';
+
+const config = {
+  css: {
+    src_dir: 'src/css',
+  },
+
+  js: {
+    src_dir: 'src/js',
+    src_file: 'src/js/index.ts',
+    out_file: 'modules.bundle.js',
+  },
+
+  html: {
+    src_dir: 'src/html',
+  },
+
+  assets: {
+    src_dir: 'src/assets',
+    out_dir: 'assets',
+  },
+
+  out_dir: 'dist',
+};
+
+export function css() {
+  return src(`${config.css.src_dir}/*.pcss`)
+    .pipe(postcss())
+    .pipe(rename({ extname: '.bundle.css' }))
+    .pipe(dest(config.out_dir));
+}
+
+export function js() {
+  return src(config.js.src_file)
+    .pipe(sourcemaps.init())
+    .pipe(
+      rollup(
+        {
+          plugins: [
+            typescript(),
+            node_resolve({
+              jsnext: true,
+              preferBuiltins: true,
+              browser: true,
+              extensions: ['.js', '.ts'],
+            }),
+            commonjs({
+              extensions: ['.js', '.ts'],
+            }),
+            terser({
+              compress: {
+                booleans_as_integers: true,
+                hoist_funs: true,
+                keep_fargs: false,
+                module: true,
+                passes: 2,
+                pure_getters: true,
+                warnings: true,
+              },
+            }),
+          ],
+        },
+        {
+          format: 'iife',
+        },
+      ),
+    )
+    .pipe(rename({ basename: 'modules', extname: '.bundle.js' }))
+    .pipe(sourcemaps.write('.', { addComment: true }))
+    .pipe(dest(config.out_dir));
+}
+
+export function sw() {
+  return sw_precache.write(`${config.out_dir}/sw.js`, {
+    staticFileGlobs: [`${config.out_dir}/**/*.{js,css,html,ttf}`],
+    stripPrefix: config.out_dir,
+  });
+}
+
+export function html(): NodeJS.ReadWriteStream {
+  return src(`${config.html.src_dir}/*.html`, { since: lastRun(html) })
+    .pipe(
+      html_minifier({
+        collapseWhitespace: true,
+        decodeEntities: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        sortAttributes: true,
+        sortClassName: true,
+        collapseBooleanAttributes: true,
+      }),
+    )
+    .pipe(dest(config.out_dir));
+}
+
+export function assets(): NodeJS.ReadWriteStream {
+  return src(`${config.assets.src_dir}/**/*`, { since: lastRun(assets) }).pipe(
+    dest(`${config.out_dir}/${config.assets.out_dir}`),
+  );
+}
+
+export function clean() {
+  return del('dist');
+}
+
+export const build = series(clean, parallel(html, js, css, assets), sw);
+
+export const watch = series(build, function watch() {
+  gulp_watch(`${config.css.src_dir}/**/*.pcss`, series(css, sw));
+  gulp_watch(`${config.html.src_dir}/**/*.html`, series(html, sw));
+  gulp_watch(`${config.js.src_dir}/**/*.(j|t)s`, series(js, sw));
+  gulp_watch(`${config.assets.src_dir}/**/*`, series(assets, sw));
+});
+
+export default build;
